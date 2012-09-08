da2c09f8544d8f09e36c1563a007f70b733c7976
Bob Florian <bflorian@gmail.com>
2012-09-08 13:17:37 -0400

    Updated explicit index documentation examples.

---
+++ test/integration/doc/ExplicitIndexesTests.groovy
@@ -0,0 +1,57 @@
+package doc
+
+import org.junit.Test
+import static org.junit.Assert.*
+import docs.explicitindexes.*
+
+/**
+ * @author: Bob Florian
+ */
+class ExplicitIndexesTests extends ExamplesBase
+{
+	static document = "../cassandra-orm/src/docs/guide/2.3 Explicit Indexes.gdoc"
+	static imports = ["import docs.explicitindexes.*"]
+
+	@Test
+	void testSetup()
+	{
+		new Person(username: "sally", emailAddress: "sally@foo.com", phone: "+13015551212", state: "Maryland", city: "Baltimore").save()
+		new Person(username: "sue", emailAddress: "sue@foo.com", phone: "+17035551111", state: "Virginia", city: "Reston").save()
+		new Person(username: "bill", emailAddress: "bill@foo.com", phone: "+17035551112", state: "Virginia", city: "Fairfax").save()
+		new Person(username: "bob", emailAddress: "bob@foo.com", phone: "+14085551111", state: "California", city: "San Francisco").save()
+		new Person(username: "mary", emailAddress: "mary@foo.com", phone: "+14085551111", state: "California", city: "San Francisco").save()
+		new Person(username: "betty", emailAddress: "betty@foo.com", phone: "+18035551111", state: "California", city: "Los Angeles").save()
+		new Person(username: "pete", emailAddress: "pete@foo.com", phone: "+13015551112", state: "Maryland", city: "Frederick").save()
+		new Person(username: "sam", emailAddress: "sam@foo.com", phone: "+13015551113", state: "Maryland", city: "Annapolis").save()
+		new Person(username: "john", emailAddress: "john@foo.com", phone: "+13015551114", state: "Maryland", city: "Annapolis").save()
+		new Person(username: "fred", emailAddress: "fred@foo.com", phone: "+13015551212", state: "Maryland", city: "Rockville").save()
+	}
+
+	@Test
+	void testFindByQueries()
+	{
+		def result = processFile(document, imports, "Person.findByPhone")
+		assertEquals 1, result[0].size()
+	}
+
+	@Test
+	void testFindWhereQueries()
+	{
+		def result = processFile(document, imports, "Person.findWhere")
+		assertEquals 1, result[0].size()
+	}
+
+	@Test
+	void testFindByCollection()
+	{
+		def result = processFile(document, imports, "Person.findAllByStateAndCity")
+		assertEquals 4, result[0].size()
+	}
+
+	@Test
+	void testCountBy()
+	{
+		def result = processFile(document, imports, "Person.countByState")
+		assertEquals 2, result[0]
+	}
+}
