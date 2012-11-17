8cded13a89b81d49c9b76c0ddfe22e2303da0f8b
Bob Florian <bflorian@gmail.com>
2012-11-17 08:22:36 -0500

    Updated WebsiteVisit class definition.

--- grails-app/cassandra/websites/WebsiteVisit.groovy
+++ grails-app/cassandra/websites/WebsiteVisit.groovy
@@ -9,17 +9,21 @@ class WebsiteVisit
 	UUID visitId
 	UUID visitorId
 	String siteId
-	Date occurTime
-	String refUrl            // Value of Referer HTTP header param
-	String refClass          // ReachLocal | Organic
-	String refType           // Direct | Search | Directory | Social | Other Website
-	String refName           // Name of know sites (i.e. Google, Bing) or domain name
-	String refKeyword        // Search expression (set for refType == Search only)
-	Boolean bounced = true   // True if no subsequent page views occured within session timeout
-	Long timeOnSite = 0L     // Occur time of last action during this visit site during that session
-	Integer totalActions = 1 // Total number of page views and posts for the visit
+	Date occurTime              // Time of HTTP request (msec since 1/1/1970)
+	String refUrl               // Value of Referer HTTP header param
+	String refClass             // Paid | Organic
+	String refType              // Direct | Search | Directory | Social | Other Website
+	String refName              // Name of know sites (i.e. Google, Bing) or domain name
+	String refKeyword           // Search expression (set for refType == Search only)
+	String pageUrl              // URL of the requested page (i.e. document.URL)
+	String pageTitle            // Title of the requested page (i.e. document.title) or the URL if title not set
+	Boolean newVisitor = true   // True for first visit by this visitor, otherwise false
+	Boolean bounced = true      // True if no subsequent page views occured within session timeout
+	Long timeOnSite = 0L        // Occur time of last action during this visit site during that session
+	Integer totalActions = 1    // Total number of page views and posts for the visit
 
-	static hasMany = [actions: WebsiteAction]
+	String refCtype = "Organic" // Product name (ReachSearch | ReachDispay | ReachRemarketing | ReachCast | Organic (for non-reachlocal))
+	String refPubName = "none"  // Publisher name (derived from sub-campaign ID) or 'none' for non-reachlocal
 
 	static cassandraMapping = [
 			unindexedPrimaryKey: 'visitId',
@@ -27,15 +31,22 @@ class WebsiteVisit
 			explicitIndexes: [
 					['siteId'],
 					['visitorId'],
-					['refClass'],
-					['refType']
+					['refType'],
+					['siteId','refClass','refType'],
 			],
 
 			counters: [
 					[findBy:['siteId'], groupBy: ['occurTime']],
-					[findBy:['siteId'], groupBy: ['occurTime','refClass','refType']],
-					[findBy:['siteId'], groupBy: ['occurTime','refClass','refType','refName']],
-					[findBy:['siteId'], groupBy: ['occurTime','refClass','refType','refName','refKeyword']]
+					[findBy: ['siteId'], groupBy: ['occurTime','refClass','refCtype','refType','newVisitor','bounced']],
+					[findBy: ['siteId'], groupBy: ['occurTime','refClass','refCtype','refType','newVisitor','bounced','refPubName','refName']],
+					[findBy: ['siteId'], groupBy: ['occurTime','refClass','refCtype','refType','newVisitor','bounced','refPubName','refName','refKeyword']],
+					[findBy: ['siteId'], groupBy: ['occurTime','refClass','refCtype','refType','newVisitor','bounced','refPubName','refName','refPage']],
+					[findBy: ['siteId'], groupBy: ['occurTime','refClass','refCtype','refType','newVisitor','bounced','pageUrl']],
+					[findBy: ['siteId'], groupBy: ['occurTime','refClass','refCtype','refType','newVisitor','bounced','pageTitle']],
+
+					[findBy: ['siteId'], groupBy: ['occurTime']],
+					[findBy: ['siteId'], groupBy: ['occurTime','newVisitor']],
+					[findBy: ['siteId'], groupBy: ['occurTime','bounced']]
 			],
 
 			keySpace: "websites"
